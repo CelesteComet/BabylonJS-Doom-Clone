4fef6c6a67ea9e74b3f7399a2d5f9ee4bc3f96ea
Bruce Wong <brucewong21@gmail.com>
2017-11-05 22:41:32 -0800

    added angular sensibility

--- globals.js
+++ globals.js
@@ -22,6 +22,9 @@ var assetsManager = new BABYLON.AssetsManager(scene);
 // create the camera
 //var camera = new BABYLON.UniversalCamera('camera1', new BABYLON.Vector3(0, 2, -2), scene);
 var camera = new BABYLON.DeviceOrientationCamera("DevOr_camera", new BABYLON.Vector3(0, 2, -2), scene);
+    // Sets the sensitivity of the camera to movement and rotation
+    camera.angularSensibility = 10;
+    camera.moveSensibility = 10;
 camera.applyGravity = true;
 camera.checkCollisions = true;
 camera.ellipsoid = new BABYLON.Vector3(0.1, 1.5, 0.2);
